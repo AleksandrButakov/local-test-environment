e1ea6542a5ee228beca02e5d6634fcf5c246ab9a
Aleksandr <anbngm@gmail.com>
2023-03-10 18:26:07 +0500

    Added CredentialsResponse.java file

---
+++ src/test/java/models/lombok/CredentialsResponse.java
@@ -0,0 +1,23 @@
+package models.lombok;
+
+import lombok.Data;
+
+@Data
+public class CredentialsResponse {
+
+    private long id;
+    private int petId;
+    private int quantity;
+    private String shipDate;
+    private String status;
+    private boolean complete;
+
+    /*
+        "id": 9222968140497185134,
+        "petId": 0,
+        "quantity": 0,
+        "shipDate": "2022-04-24T12:46:04.571+0000",
+        "status": "placed",
+        "complete": true
+     */
+}
