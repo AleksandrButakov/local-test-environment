9b5a12058335ee9f62a13300adfdaf6dae4b947e
Aleksandr <anbngm@gmail.com>
2023-03-22 20:03:24 +0500

    Added test

--- src/test/java/tests/TestAPI.java
+++ src/test/java/tests/TestAPI.java
@@ -92,4 +92,59 @@ public class TestAPI {
         assertThat(credentialsResponse.getStatus()).isEqualTo("placed");
     }
 
+
+    @Test
+    @DisplayName("Swagger Petstore POST test")
+    void swaggerPetstorePostTest02ForEdit() {
+        /* String data = "{ \"id\": 0, " +
+                "\"petId\": 0, " +
+                "\"quantity\": 0, " +
+                "\"shipDate\": \"2022-04-24T12:46:04.571Z\", " +
+                "\"status\": \"placed\", " +
+                "\"complete\": true }";
+         */
+
+        Credentials credentials = new Credentials();
+        credentials.setId(0);
+        credentials.setPetId(0);
+        credentials.setQuantity(0);
+        credentials.setShipDate("2022-04-24T12:46:04.571Z");
+        credentials.setStatus("placed");
+        credentials.setComplete(true);
+
+        CredentialsResponse credentialsResponse =
+                given()
+                        // .filter(new AllureRestAssured())
+                        .filter(withCustomTemplates())
+                        .contentType(ContentType.JSON)
+                        .body(credentials)
+                        // .log().all()
+                        .log().method()
+                        .log().uri()
+                        .log().headers()
+                        .log().body()
+                        .when()
+                        .post("/v2/store/order")
+                        .then()
+                        // .log().all()
+                        .log().status()
+                        .log().body()
+                        .statusCode(200)
+                        .body(matchesJsonSchemaInClasspath("schemas/Generate_response_scheme.json"))
+                        //.body("status", is("placed"));
+                        .extract().as(CredentialsResponse.class);
+
+        /*
+            "id": 9222968140497185134,
+            "petId": 0,
+            "quantity": 0,
+            "shipDate": "2022-04-24T12:46:04.571+0000",
+            "status": "placed",
+            "complete": true
+        */
+        assertThat(credentialsResponse.getId()).isNotZero();
+        assertThat(credentialsResponse.getShipDate()).hasSizeGreaterThan(12);
+        assertThat(credentialsResponse.getStatus()).isEqualTo("placed");
+    }
+
 }
