0c92981ede067a098c5441255649d292435a600b
AleksandrButakov <anbngm@gmail.com>
2023-04-21 20:04:34 +0500

    Working class

--- src/test/java/tests/demoqa/TestBase.java
+++ src/test/java/tests/demoqa/TestBase.java
@@ -9,39 +9,21 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
 
 class TestBase {
 
     @BeforeAll
     static void beforeAll() throws MalformedURLException {
-//        Configuration.baseUrl = "https://demoqa.com";
-//        Configuration.browser = "chrome";
-//        Configuration.browserVersion = "112.0";
-//        Configuration.browserSize = "1920x1080";
-//        Configuration.remote = "http://62.113.108.218:4444/wd/hub";
-//
-//        DesiredCapabilities capabilities = new DesiredCapabilities();
-//
-//        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
-//                "enableVNC", true,
-//                "enableVideo", true
-//        ));
-//        Configuration.browserCapabilities = capabilities;
-
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
 
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
         Configuration.remote = "http://62.113.108.218:4444/wd/hub";
 
-        //ChromeOptions options = new ChromeOptions();
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", new HashMap<String, Object>() {{
             /* How to add test badge */
@@ -52,33 +34,18 @@ class TestBase {
 
             /* How to set timezone */
             put("env", new ArrayList<String>() {{
-                add("TZ=UTC+5");
+                add("TZ=UTC");
             }});
 
             /* How to enableVNC */
             put("enableVNC", true);
         }});
+
         Configuration.browserCapabilities = capabilities;
-
-
-
-
-
-//        DesiredCapabilities capabilities = new DesiredCapabilities();
-//        //capabilities.setCapability("enableVNC", true);
-//        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
-//                "enableVideo", true
-//        ));
-
-        //Configuration.browserCapabilities = capabilities;
-
-
-
     }
 
     @BeforeEach
     void addListener() {
-
     }
 
     @AfterEach
@@ -88,4 +55,5 @@ class TestBase {
         Attach.browserConsoleLogs();
         Attach.addVideo();
     }
+
 }
