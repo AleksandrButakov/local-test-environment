18f2821b016c157e1297b01af4a3ae482d197949
AleksandrButakov <anbngm@gmail.com>
2023-04-21 17:16:38 +0500

    temp

--- src/test/java/tests/demoqa/TestBase.java
+++ src/test/java/tests/demoqa/TestBase.java
@@ -37,26 +37,26 @@ class TestBase {
 
         ChromeOptions options = new ChromeOptions();
 
-//        options.setCapability("selenoid:options", new HashMap<String, Object>() {{
-//            /* How to add test badge */
-//            put("name", "Test badge...");
-//
-//            /* How to set session timeout */
-//            put("sessionTimeout", "15m");
-//
-//            /* How to set timezone */
-//            put("env", new ArrayList<String>() {{
-//                add("TZ=UTC");
-//            }});
-//
-//            /* How to add "trash" button */
-//            put("labels", new HashMap<String, Object>() {{
-//                put("manual", "true");
-//            }});
-//
-//            /* How to enable video recording */
-//            put("enableVideo", true);
-//        }});
+        options.setCapability("selenoid:options", new HashMap<String, Object>() {{
+            /* How to add test badge */
+            put("name", "Test badge...");
+
+            /* How to set session timeout */
+            put("sessionTimeout", "15m");
+
+            /* How to set timezone */
+            put("env", new ArrayList<String>() {{
+                add("TZ=UTC");
+            }});
+
+            /* How to add "trash" button */
+            put("labels", new HashMap<String, Object>() {{
+                put("manual", "true");
+            }});
+
+            /* How to enable video recording */
+            put("enableVideo", true);
+        }});
 
 
 
